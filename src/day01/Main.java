1d1164d3348d5ee086cb1290555042991289498b
Lui-die-lui <lsg05h@naver.com>
2025-05-14 19:14:19 +0900

    init: first commit

--- src/day01/Main.java
+++ src/day01/Main.java
@@ -3,6 +3,7 @@ package day01;
 public class Main {
     public static void main(String[] args) {
        System.out.println("hello");
+       System.out.println("world");
     // ctrl+/= 주석처리 수동 //=주석처리 (선택 범위 내 가능)
     }
 }
