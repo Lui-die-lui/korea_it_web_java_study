9a4843696ae27b4b586854f38c01758646cdff6e
Lui-die-lui <lsg05h@naver.com>
2025-05-19 21:48:19 +0900

    Feat: 이중 반복문, 구구단 만들기

--- src/_11_NestedLoop/NestedLoop.java
+++ src/_11_NestedLoop/NestedLoop.java
@@ -14,9 +14,8 @@ public class NestedLoop {
         // *****
 
 
-
-        for (int i = 0; i < 5 ; i++) { // 세로줄
-            for (int j = 0; j < 5 ; j++) { // 가로로 별찍기
+        for (int i = 0; i < 5; i++) { // 세로줄
+            for (int j = 0; j < 5; j++) { // 가로로 별찍기
                 System.out.print("*");
             }
             System.out.println();// 줄바꿈
@@ -30,22 +29,59 @@ public class NestedLoop {
         //*****
         System.out.println();
 
-        for (int i = 0; i < 5 ; i++) {
-            for (int j = 0 ; j < i + 1 ; j++) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i + 1; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
 
-        for (int i = 0 ; i < 5 ; i++) {
-            for (int j = 0 ; j < i + 5 ; j++) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i + 5; j++) {
                 System.out.print(".");
             }
             System.out.println();
-
         }
+
+
+        //*****
+        //****
+        //***
+        //**
+        //*
+
+        // 10 ~ 1 까지 거꾸로 출력
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+        }
+//
+//        for (int i = 5; i >= 1; i--) { //5번 반복 5 4 3 2 1
+//            for (int j = 1; j < i; j--) { //1개찍 감소 - 후에 적용되는 값 i = 5, 4, 3, 2, 1 로 작아질때마다
+//                System.out.print("*"); // 확인 이상함
+
+
+
+
+    /*
+    SSSS* i = 0, j 4번 j < 4
+    SSS** i = 1, j 3번 j < 3
+    SS*** i = 2, j 2번 j < 2
+    S****
+    *****
+     */
+
+        for (int i = 0; i < 5 ; i++) {
+            for (int j = 0; j < 4 - i ; j++) {
+                System.out.print("S");
+            }
+
+            for (int j = 0; j < i + 1 ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
 
 
     }
+}
 
---
+++ src/_12_MultipleTable/MultipleTable.java
@@ -0,0 +1,17 @@
+package _12_MultipleTable;
+
+public class MultipleTable {
+    public static void main(String[] args) {
+        //1~9단까지 출력하십시오
+        //1 X 3  = 3
+        for (int i = 1 ; i < 10 ; i++) { // 단
+            for (int j = 1; j < 10 ; j++) { // 곱해지는 값 '뒷쪽'
+                System.out.println(i + "X" + j + "=" + i*j);
+
+            }
+            System.out.println();
+
+
+        }
+    }
+}
